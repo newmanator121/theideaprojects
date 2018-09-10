37ca473cbfa8b254bcae9e88c92dc42f194d2415
newmanator121 <alexander.newman@unb.ca>
2018-09-10 16:58:14 -0300

    Adding base directories for multiple projects

--- Java Projects/src/INFO3103/HTTP.java
+++ Java Projects/src/INFO3103/HTTP.java
@@ -1,44 +1,30 @@
-//package INFO3103;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+package INFO3103;
+import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
+import java.nio.file.*;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.Scanner;
+
 
 public class HTTP {
 
     public static void main(String[] args) {
+        /**
+         * Global Variables
+         */
         Scanner scan = new Scanner(System.in);
         String protocol;
         String domainName;
         String documentPath;
         int portNum;
-        CharSequence compare1 = "http";
-        CharSequence compare2 = "https";
-        String command;
 
 
 
         //Getting the Protocol
         System.out.println("Please enter either https or https all lowercase   ");
         protocol = scan.next();
-
-        /**
-         if(protocol.contains(compare1) == false) {
-         System.out.println("Please enter either HTTP or HTTPS   ");
-         protocol =scan.next();
-         }
-
-         else if(protocol.contains(compare2) == false) {
-         System.out.println("Please enter either HTTP or HTTPS   ");
-         protocol =scan.next();
-         }
-
-         else{}
-         */
 
         //Getting the domain
         System.out.println("Please enter the domain name, EX: wizards.cs.unb.ca ");
@@ -53,6 +39,7 @@ public class HTTP {
         portNum = scan.nextInt();
 
         /**
+         * Used for error checking
         System.out.println(protocol);
         System.out.println(documentPath);
         System.out.println(portNum);
@@ -65,34 +52,26 @@ public class HTTP {
             URL theURL = new URL(protocol, domainName, portNum, documentPath);
             System.out.println("HERE    " + theURL.toString());
             BufferedReader in = new BufferedReader(new InputStreamReader(theURL.openStream()));
-            String[] terminalCommand = new String[] {"xterm -hold -e ","wget -O AlexTest.html " + theURL.toString()};
 
+            //Print out the webpage into the terminal
             String input;
             while((input = in.readLine()) != null) {
                 System.out.println(input);
             }
             in.close();
 
-            ProcessBuilder pb = new ProcessBuilder("xterm","wget -O ~/Desktop/AlexTest.html" + theURL.toString());
-            Process p = pb.start();
-            p.waitFor();
+            //Will open a stream to the webpage using URL java methods and puts it into the users Desktop directory
+            Path pth = Paths.get(System.getProperty("user.home"),"Desktop", "index.html.html");
+            InputStream urlFileIn = theURL.openStream();
+            Files.copy(urlFileIn,pth,StandardCopyOption.REPLACE_EXISTING);
+
 
         }catch(MalformedURLException e) {
-            System.out.println("Caught a bad URL OH No!" + e.getMessage());
+            System.out.println("Something is wrong with the URL " + e.getMessage());
         }
         catch(IOException IOE) {
-            System.out.println("Caught a bad URL Connection OH No!" + IOE.getMessage());
+            System.out.println("Something threw an I/O exception  " + IOE.getMessage());
         }
-        catch(InterruptedException IOE) {
-            System.out.println("Interrupt   " + IOE.getMessage());
-        }
-
-        //Grab the file using wget
-       // Runtime newTerminal = new Runtime.getRuntime();
-        //newTerminal.exec("wget" + theURL.get )
-        //wget -O index.html [URL]
-
-
     }//end of main
 
 }//end of class
