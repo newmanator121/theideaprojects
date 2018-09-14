0ad9b7868db7ad93dc0feb1cb3ddf5239bc3dddc
newmanator121 <alexander.newman@unb.ca>
2018-09-14 16:44:05 -0300

    Adding base directories for multiple projects

---
+++ Java Projects/src/ArduinoProject/Interface.java
@@ -0,0 +1,18 @@
+package ArduinoProject;
+
+import java.io.*;
+import java.util.*;
+import javax.*;
+
+
+public class Interface {
+
+
+    public static void main(String args[]) {
+
+
+
+    }
+
+
+}
--- Java Projects/src/INFO3103/Assignment1/URLClass.java
+++ Java Projects/src/INFO3103/Assignment1/URLClass.java
@@ -1,4 +1,4 @@
-//package INFO3103.Assignment1;
+package INFO3103.Assignment1;
 
 import java.io.BufferedReader;
 import java.io.IOException;
