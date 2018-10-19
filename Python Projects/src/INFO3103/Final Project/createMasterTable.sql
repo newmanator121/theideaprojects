9ead3e14e4c93591a957d203b87d4809120ea60c
newmanator121 <alexander.newman@unb.ca>
2018-10-19 18:03:08 -0300

    Adding SQL stored Procedures

--- Python Projects/src/INFO3103/Final Project/createMasterTable.sql
+++ Python Projects/src/INFO3103/Final Project/createMasterTable.sql
@@ -3,7 +3,5 @@ CREATE TABLE masterTable (
   userID INT            NOT NULL AUTO_INCREMENT,
   owner     varchar255  NOT NULL,
   list varchar(20)      DEFAULT NULL,
-  item varchar(40000)   DEFAULT NULL,
-  level    varchar(10)  DEFAULT NULL,
   PRIMARY KEY (userID)
 );
